2127fab24b64cbcfc85949c7d805fe4f51a336d6
friedpies <kenmarut@gmail.com>
2019-06-06 16:05:52 -0500

    broke generation down into 10 parts

--- generator.js
+++ generator.js
@@ -2,7 +2,8 @@ const faker = require("faker");
 const skuGen = require("shortid");
 const fs = require("fs");
 
-const numRecordsToGenerate = 10000;
+const numRecordsToGenerate = 100000;
+const numberOfSeparateFiles = 10;
 
 const generateRandomColorArray = upTo => {
   const numColors = Math.ceil(upTo * Math.random());
@@ -25,61 +26,67 @@ const generateRandomImages = upTo => {
 
   return images;
 };
-
-const categoryString = "sku,productName,category,color,price,images\n";
-try {
-  fs.writeFileSync("./records.csv", categoryString);
-  console.log("HEADERS ADDED");
-} catch (err) {
-  console.log("THERE WAS AN ERROR WRITING HEADERS");
-}
-
-for (let entry = 0; entry < numRecordsToGenerate; entry++) {
-  if (entry % 10 === 0) {
-    // LOG PERCENTAGE COMPLETED, ONLY 1 OUT OF 10 ITERATIONS
-    console.log(100 * (entry / numRecordsToGenerate) + "% COMPLETE");
-  }
-  let shoe = {
-    sku: skuGen.generate() + "",
-    productName: faker.commerce.productName(),
-    category: faker.commerce.department(),
-    color: generateRandomColorArray(20),
-    price: (150 * Math.random()).toFixed(2),
-    images: generateRandomImages(12)
-  };
-
-  let currentEntry = "";
-
-  for (let key in shoe) {
-    if (key === "color" || key === "images") {
-      currentEntry += '"';
-      for (let i = 0; i < shoe[key].length; i++) {
-        currentEntry += shoe[key][i];
-        if (i !== shoe[key].length - 1) {
-          currentEntry += ",";
-        }
-      }
-      currentEntry += '"';
-    } else {
-      currentEntry += shoe[key];
-    }
-    currentEntry += ",";
-  }
-  currentEntry = currentEntry.substr(0, currentEntry.length - 1); // remove last comma and replace it with a new line char
-  currentEntry += "\n";
-  // fs.appendFileSync("./records.csv", currentEntry, err => {
-  //   if (err) {
-  //     console.error(err);
-  //   } else {
-  //   }
-  // });
-
+for (let fileNumber = 0; fileNumber < numberOfSeparateFiles; fileNumber++) {
+  const categoryString = "sku,productName,category,color,price,images\n";
   try {
-    fs.appendFileSync("./records.csv", currentEntry);
-    // console.log("HEADERS ADDED");
+    fs.writeFileSync(`./records${fileNumber}.csv`, categoryString);
+    console.log("HEADERS ADDED");
   } catch (err) {
-    console.error(err);
+    console.log("THERE WAS AN ERROR WRITING HEADERS");
+  }
+
+  for (
+    let entry = 0;
+    entry < numRecordsToGenerate / numberOfSeparateFiles;
+    entry++
+  ) {
+    if (entry % 1000 === 0) {
+      // LOG PERCENTAGE COMPLETED, ONLY 1 OUT OF 10 ITERATIONS
+      console.clear();
+      console.log(
+        `File Number: ${fileNumber + 1} of ${numberOfSeparateFiles}: ` +
+          (
+            100 *
+            (entry / (numRecordsToGenerate / numberOfSeparateFiles))
+          ).toFixed(0) +
+          "%"
+      );
+    }
+
+    let shoe = {
+      sku: skuGen.generate() + "",
+      productName: faker.commerce.productName(),
+      category: faker.commerce.department(),
+      color: generateRandomColorArray(20),
+      price: (150 * Math.random()).toFixed(2),
+      images: generateRandomImages(12)
+    };
+
+    let currentEntry = "";
+
+    for (let key in shoe) {
+      if (key === "color" || key === "images") {
+        currentEntry += '"';
+        for (let i = 0; i < shoe[key].length; i++) {
+          currentEntry += shoe[key][i];
+          if (i !== shoe[key].length - 1) {
+            currentEntry += ",";
+          }
+        }
+        currentEntry += '"';
+      } else {
+        currentEntry += shoe[key];
+      }
+      currentEntry += ",";
+    }
+    currentEntry = currentEntry.substr(0, currentEntry.length - 1); // remove last comma and replace it with a new line char
+    currentEntry += "\n";
+
+    try {
+      fs.appendFileSync(`./records${fileNumber}.csv`, currentEntry);
+      // console.log("HEADERS ADDED");
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
-
-console.log();
